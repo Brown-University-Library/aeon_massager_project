f3067d16e524d17974622d805386fb2d1244c5dc
birkin <birkin.diana@gmail.com>
2021-07-27 14:23:41 -0400

    checks reload issue; all good.

--- aeon_massager_app/views.py
+++ aeon_massager_app/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 
 
 def info(request):
-    return HttpResponse("Hello, world. You're at the info-I page.")
-    # resp = render( request, 'info.html', {'foo': 'CC'} )
+    # return HttpResponse("Hello, world. You're at the info-I page.")
+    resp = render( request, 'info.html', {'foo': 'DD'} )
     return resp
